84892e3ef9373bccdb9ef48a87f7b6295bf2e9de
tamiresescobar1 <124881009+tamiresescobar1@users.noreply.github.com>
2024-04-29 18:04:57 +0100

    feat: component widget

--- src/components/Card/Card.styled.tsx
+++ src/components/Card/Card.styled.tsx
@@ -2,9 +2,7 @@
 import { Paper } from '@mui/material'
 import { styled } from '@mui/material/styles'
 
-export const CardStyled = styled(Paper)`
-  ${({ theme }) => `
-    height: auto;
-    padding: ${theme.spacing()}
-  `}
-`
+export const CardStyled = styled(Paper)(({ theme }) => ({
+  height: 'auto',
+  padding: theme.spacing(2),
+}));
