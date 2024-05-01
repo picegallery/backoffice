502bd14f4d8a2c664398f4dcfcbed9695a9833e7
Débora Gonçalves <debora.crishsm2@hotmail.com>
2024-05-01 21:46:55 +0100

    fixes (#29)
    * feat: Pg 167 fe component button (#22)
    
    * feat: added style button and fixed the icon color
    
    * feat: button adjustment
    
    * feat: change and correction
    
    * feat: correction
    
    * feat: fix imports
    
    * feat: artist form (#24)
    
    * feat(artist form): artist form
    
    * feat(pg-122): adicionado input image, textarea, form toolbar
    
    * feat(): artist form
    
    * artist form (#25) (#26)
    
    * feat: Pg 167 fe component button (#22)
    
    * feat: added style button and fixed the icon color
    
    * feat: button adjustment
    
    * feat: change and correction
    
    * feat: correction
    
    * feat: fix imports
    
    * feat: artist form (#24)
    
    * feat(artist form): artist form
    
    * feat(pg-122): adicionado input image, textarea, form toolbar
    
    * feat(): artist form
    
    ---------
    
    Co-authored-by: tamiresescobar1 <124881009+tamiresescobar1@users.noreply.github.com>
    
    * fix(): added ts ignore
    
    * fix(): types for build
    
    ---------
    
    Co-authored-by: tamiresescobar1 <124881009+tamiresescobar1@users.noreply.github.com>

--- src/components/TableActions/TableActions.stories.tsx
+++ src/components/TableActions/TableActions.stories.tsx
@@ -1,56 +1,13 @@
 import { Meta, StoryObj } from '@storybook/react'
 import { within } from '@storybook/testing-library'
 import { expect } from '@storybook/jest'
-import DataTable from './TableActions'
-import { GridColDef } from '@mui/x-data-grid'
+import TableActions from './TableActions'
 
-const columns: GridColDef[] = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`
-  }
-]
-
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 }
-]
-
-const meta: Meta<typeof DataTable> = {
-  title: 'DataTable',
-  argTypes: {
-    rows: { type: 'string' },
-    columns: { type: 'string' },
-    pageSize: { type: 'number' },
-    pageNumber: { type: 'number' }
-  },
-  args: {
-    rows,
-    columns,
-    pageSize: 5,
-    pageNumber: 0
-  },
-  component: DataTable
+const meta: Meta<typeof TableActions> = {
+  title: 'TableActions',
+  argTypes: {},
+  args: {},
+  component: TableActions
 }
 
 export default meta
@@ -59,6 +16,6 @@ type Story = StoryObj<typeof meta>
 export const Default: Story = {
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement)
-    expect(canvas.getByTestId('datatable-component')).toBeDefined()
+    expect(canvas.getByTestId('table-actions-component')).toBeDefined()
   }
 }
